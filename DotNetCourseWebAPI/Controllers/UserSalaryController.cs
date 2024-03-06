a8ebc8f6bf1e4a3402f7fb17f1a4b7b681b32821
iliaurushadze8 <iliaurushadze24@gmail.com>
2024-03-06 08:05:55 +0400

    Added user salary controller.

---
+++ DotNetCourseWebAPI/Controllers/UserSalaryController.cs
@@ -0,0 +1,21 @@
+﻿using DotNetCourseWebAPI.Data;
+using DotNetCourseWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCourseWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserSalaryController : ControllerBase
+    {
+        private readonly DataContextDapper _dapper;
+
+        public UserSalaryController(IConfiguration config)
+        {
+            _dapper = new DataContextDapper(config);
+        }
+
+        
+    }
+}
