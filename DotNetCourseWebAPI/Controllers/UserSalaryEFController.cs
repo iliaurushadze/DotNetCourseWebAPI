9bcca509396dd776b01b9e4f93df65c0e35ce8bd
iliaurushadze8 <iliaurushadze24@gmail.com>
2024-03-07 20:18:30 +0400

    Added user salary ef controller

---
+++ DotNetCourseWebAPI/Controllers/UserSalaryEFController.cs
@@ -0,0 +1,19 @@
+﻿using DotNetCourseWebAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCourseWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserSalaryEFController : ControllerBase
+    {
+        private readonly DataContextEF _ef;
+        public UserSalaryEFController(IConfiguration config)
+        {
+            _ef = new DataContextEF(config);
+        }
+
+
+    }
+}
