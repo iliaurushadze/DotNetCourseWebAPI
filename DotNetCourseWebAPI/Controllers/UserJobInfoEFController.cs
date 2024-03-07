c98c9f8ad6f405cc059cb41d11533bdb8d8695e9
iliaurushadze8 <iliaurushadze24@gmail.com>
2024-03-07 19:16:36 +0400

    Added user job info ef controller.

---
+++ DotNetCourseWebAPI/Controllers/UserJobInfoEFController.cs
@@ -0,0 +1,20 @@
+﻿using DotNetCourseWebAPI.Data;
+using DotNetCourseWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCourseWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserJobInfoEFController : ControllerBase
+    {
+        private readonly DataContextEF _ef;
+        public UserJobInfoEFController(IConfiguration config)
+        {
+            _ef = new DataContextEF(config);
+        }
+
+        
+    }
+}
