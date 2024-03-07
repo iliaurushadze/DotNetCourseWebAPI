1a2d6b92c60ba12a215d7fcea505c823c10e513c
iliaurushadze8 <iliaurushadze24@gmail.com>
2024-03-07 20:22:37 +0400

    Added get single user salary method in user salary ef controller

--- DotNetCourseWebAPI/Controllers/UserSalaryEFController.cs
+++ DotNetCourseWebAPI/Controllers/UserSalaryEFController.cs
@@ -2,6 +2,7 @@
 using DotNetCourseWebAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace DotNetCourseWebAPI.Controllers
 {
@@ -20,6 +21,21 @@ namespace DotNetCourseWebAPI.Controllers
         {
             IEnumerable<UserSalary> userSalaries = _ef.UserSalary.ToList();
             return userSalaries;
+        }
+
+        [HttpGet("GetSingleUserSalary/{userId}")]
+        public UserSalary GetSingleUserSalary(int userId)
+        {
+            UserSalary? userSalary = _ef.UserSalary
+                .Where(u => u.UserId == userId)
+                .FirstOrDefault<UserSalary>();
+
+            if (userSalary != null)
+            {
+                return userSalary;
+            }
+
+            throw new Exception("Failed to get user salary");
         }
     }
 }
