71b23dd59a3030a73d97f97b6c0e029d3b70c5d4
iliaurushadze8 <iliaurushadze24@gmail.com>
2024-03-08 11:47:17 +0400

    Added UserRepository.

---
+++ DotNetCourseWebAPI/Data/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿
+using DotNetCourseWebAPI.Models;
+
+namespace DotNetCourseWebAPI.Data
+{
+    public class UserRepository
+    {
+        private readonly DataContextEF _ef;
+
+        public UserRepository(IConfiguration config)
+        {
+            _ef = new DataContextEF(config);
+        }
+
+        public bool SaveChanges()
+        {
+            return _ef.SaveChanges() > 0;
+        }
+
+        public void AddEntity<T>(T entityToAdd)
+        {
+            if (entityToAdd != null)
+                _ef.Add(entityToAdd);
+        }
+
+        public void RemoveEntity<T>(T entityToRemove)
+        {
+            if(entityToRemove != null)
+                _ef.Remove(entityToRemove);
+        }
+    }
+}
