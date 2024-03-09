db7e7308a1328d86d7f2b187ee4b96477e3db9cc
iliaurushadze8 <iliaurushadze24@gmail.com>
2024-03-09 10:42:50 +0400

    Added Auth controller.

---
+++ DotNetCourseWebAPI/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using DotNetCourseWebAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCourseWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly DataContextDapper _dapper;
+        private readonly IConfiguration _config;
+
+        public AuthController(IConfiguration config)
+        {
+            _dapper = new DataContextDapper(config);
+            _config = config;
+        }
+
+
+    }
+}
